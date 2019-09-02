034fdc60aa094067407f803081236ad1e5e6f41c
wences <villegaswences@gmail.com>
2019-09-02 12:07:38 -0300

    agregados clases test

---
+++ src/CarritoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarritoTest {
+
+    @org.junit.jupiter.api.Test
+    void agregarProductoCarrito() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void eliminarProductoCarrito() {
+    }
+}
---
+++ src/DetalleCarritoTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DetalleCarritoTest {
+
+    @org.junit.jupiter.api.Test
+    void getProductos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setProductos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCantidad() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCantidad() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProducto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setProducto() {
+    }
+}
