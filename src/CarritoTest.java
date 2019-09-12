f6878b746d52d14bfa6a7743045c79c06d00325a
wences <villegaswences@gmail.com>
2019-09-12 15:12:04 -0300

    test wences por octava vez

--- src/CarritoTest.java
+++ src/CarritoTest.java
@@ -15,6 +15,26 @@ class CarritoTest {
     }
 
     @Test
+    void crearDetallePorProducto(){
+        int cantidadDetallesEsperada = 2;
+        Producto televisor = new Producto(1, "Televisor", "televisor LED", 4);
+        Carrito carrito = new Carrito(televisor, 2);
+        Producto microondas = new Producto(2, "Microondas", "MO-215", 12);
+        carrito.agregarProductoCarrito(microondas,2);
+        assertEquals(cantidadDetallesEsperada, carrito.getDetallesCarrito().size());
+    }
+
+    @Test
+    void agregarProductoExistenteEnCarrito(){
+        int cantidadTelevisoresEsperada = 8;
+        Producto televisor = new Producto(1, "Televisor", "televisor LED", 14);
+        Carrito carrito = new Carrito(televisor, 3);
+        carrito.agregarProductoCarrito(televisor, 5);
+        assertEquals(cantidadTelevisoresEsperada, carrito.getDetallesCarrito().get(0).getCantidad());
+    }
+
+
+    @Test
     void eliminarProductoCarrito() {
     }
 }
